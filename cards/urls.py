a921f834857d51ca0a8d98c6d35565c4cf4f7f66
Maxoman13 <maxxorosh@mail.ru>
2024-03-10 23:03:52 +0300

    Настроили пути и функции отображения

---
+++ cards/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from cards import views
+
+urlpatterns = [
+    path('catalog/', views.catalog, name='catalog'),
+    path('catalog/<int:card_id>/', views.get_card_by_id, name='card_by_id'),
+    path('catalog/<slug:slug>/', views.get_category_by_name, name='category_by_name'),
+]
--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def catalog(request):
+    return HttpResponse("Каталог карточек")
+
+
+def get_card_by_id(request, card_id):
+    return HttpResponse(f"Карточка {card_id}")
+
+
+def get_category_by_name(request, slug):
+    return HttpResponse(f"Категория {slug}")
