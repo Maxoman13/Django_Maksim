0534d80942eb9efdda905d32bc81ad27dd242281
Maxoman13 <maxxorosh@mail.ru>
2024-03-30 21:05:20 +0300

    Начало новых моделей

--- cards/models.py
+++ cards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Card(models.Model):
-    question = models.CharField(max_length=255)
+    question = models.CharField(max_length=255, db_column='Question')
     answer = models.TextField(max_length=5000)
     upload_date = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
