1b76079e3391df6364f3fc26068e18bd72a6a6fd
Maxoman13 <maxxorosh@mail.ru>
2024-04-08 21:20:29 +0300

    Добавили filter

--- cards/admin.py
+++ cards/admin.py
@@ -23,13 +23,15 @@ class CardCodeFilter(SimpleListFilter):
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'question', 'answer', 'category', 'views', 'check_status', 'upload_date')
+    list_display = ('id', 'question', 'category', 'views', 'check_status', 'upload_date')
 
     list_display_links = ('id',)
 
-    list_filter = ('category', 'upload_date')
+    list_filter = ('category', 'upload_date', CardCodeFilter)
 
     list_editable = ('views', 'question', 'check_status')
+
+    list_per_page = 10
 
     actions = ['make_checked', 'make_unchecked']
 
