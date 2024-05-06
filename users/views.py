df0812e6e4d1a2cf85fad42a90b4967bc9057de9
Maxoman13 <maxxorosh@mail.ru>
2024-05-06 22:24:30 +0300

    Подключили новое меню с пользователями

--- users/views.py
+++ users/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.urls import reverse_lazy, reverse
+
+from .forms import LoginUserForm
 
 
 def login_user(request):
-    # Здесь будет реализация входа
-    return HttpResponse("Вы вошли в систему")
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.POST.get('next', '').strip()  # Получаем next или пустую строку
+                if next_url:  # Если next_url не пустой
+                    return redirect(next_url)  # Перенаправляем на next_url
+                return redirect(reverse_lazy('catalog'))  # Перенаправляем на каталог, если next_url пуст
+            else:
+                form.add_error(None, 'Неверное имя пользователя или пароль')
+    else:
+        form = LoginUserForm()
+    return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
-    # Здесь будет реализация выхода
-    return HttpResponse("Вы вышли из системы")
+    # Вызов функции logout для завершения сессии пользователя
+    logout(request)
+    # Перенаправление на страницу входа, используя reverse для получения URL по имени
+    return redirect(reverse('users:login'))
 
 
 def signup_user(request):
