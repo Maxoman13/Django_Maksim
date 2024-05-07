8380df813db6484fabf7c8ad2f42089273d6ac02
Maxoman13 <maxxorosh@mail.ru>
2024-05-07 21:32:37 +0300

    Сделали класс для регистрации

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
 
 from cards.views import MenuMixin
 from .forms import LoginUserForm, RegisterUserForm
@@ -24,17 +25,11 @@ class LogoutUser(LogoutView):
     next_page = reverse_lazy('users:login')
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])  # Устанавливаем пароль
-            user.save()
-            return redirect('users:thanks_user')  # Перенаправляем на страницу успешной регистрации
-    else:
-        form = RegisterUserForm()  # Пустая форма для GET-запроса
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm  # Указываем класс формы, который мы создали для регистрации
+    template_name = 'users/register.html'  # Путь к шаблону, который будет использоваться для отображения формы
+    extra_context = {'title': 'Регистрация'}  # Дополнительный контекст для передачи в шаблон
+    success_url = reverse_lazy('users:thanks_user')  # URL, на который будет перенаправлен пользователь после успешной регистрации
 
 
 def thanks(request):
