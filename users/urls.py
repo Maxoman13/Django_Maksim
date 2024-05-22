c65affa93f689cf3c9a7f078c0c1804501aeccb6
Maxoman13 <maxxorosh@mail.ru>
2024-05-22 21:39:25 +0300

    Доделали сброс пароля

--- users/urls.py
+++ users/urls.py
@@ -14,28 +14,29 @@ urlpatterns = [
     path('password_change/done/', views.UserPasswordChangeDone.as_view(), name='password_change_done'),
     path("profile_cards/", views.UserCardsView.as_view(), name='profile_cards'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-             template_name='users/password_reset_form.html',
-             email_template_name='users/password_reset_email.html',
-             success_url=reverse_lazy('password_reset_done')
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+        template_name="users/password_reset_form.html",
+        email_template_name="users/password_reset_email.html",
+        success_url=reverse_lazy("users:password_reset_done"),
+    ), name="password_reset",
          ),
-         name='password_reset'),
 
-    path('password_reset_done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html'
+    # Маршрут для подтверждения сброса пароля
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="users/password_reset_done.html"
+    ), name="password_reset_done",
          ),
-         name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'
+    # Маршрут для ввода нового пароля
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/password_reset_confirm.html",
+        success_url=reverse_lazy("users:password_reset_complete"),
+    ), name="password_reset_confirm",
          ),
-         name='password_reset_confirm'),
 
-    path('reset_done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='users/password_reset_complete.html'
+    # Маршрут для завершения сброса пароля
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/password_reset_complete.html"
+    ), name="password_reset_complete",
          ),
-         name='password_reset_complete'),
 ]
